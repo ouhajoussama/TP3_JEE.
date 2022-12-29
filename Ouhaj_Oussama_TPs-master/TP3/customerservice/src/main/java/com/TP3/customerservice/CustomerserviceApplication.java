2a2a2cbab56d2a7205eebb860c1f75f29e3af341
ouhajoussama <76974169+ouhajoussama@users.noreply.github.com>
2022-12-29 14:46:57 +0100

    Update CustomerserviceApplication.java

--- Ouhaj_Oussama_TPs-master/TP3/customerservice/src/main/java/com/TP3/customerservice/CustomerserviceApplication.java
+++ Ouhaj_Oussama_TPs-master/TP3/customerservice/src/main/java/com/TP3/customerservice/CustomerserviceApplication.java
@@ -19,9 +19,9 @@ public class CustomerserviceApplication {
 	CommandLineRunner start (CustomerRepository customerRepository, RepositoryRestConfiguration restConfiguration){
 		restConfiguration.exposeIdsFor(Customer.class);
 		return args -> {
-			customerRepository.save(new Customer(null,"Anas","contact@anas.ma"));
-			customerRepository.save(new Customer(null,"Soufiane","contact@soufiane.ma"));
-			customerRepository.save(new Customer(null,"Someting","contact@someting.ma"));
+			customerRepository.save(new Customer(null,"Ouhaj","ouhaj@gmail.com"));
+			customerRepository.save(new Customer(null,"LOLO","LOLO@gmail.com"));
+			customerRepository.save(new Customer(null,"Soso","soso@gmail.com"));
 			customerRepository.findAll().forEach(System.out::println);
 		};
 	}
